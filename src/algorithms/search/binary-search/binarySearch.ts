dd6ff70298c5f8ef6b0e03f84b016612cbcc1baf
qiushiming <qiushiming1997@gmail.com>
2022-04-06 14:54:09 +0800

    feat: add binary search

--- src/algorithms/search/binary-search/binarySearch.ts
+++ src/algorithms/search/binary-search/binarySearch.ts
@@ -1,8 +1,13 @@
-function binarySearch(sortedArray: number[], seekElement: number): number {
+export default function binarySearch(
+  sortedArray: number[],
+  seekElement: number
+): number {
   let startIndex = 0;
   let endIndex = sortedArray.length - 1;
 
   while (startIndex <= endIndex) {
+    // const middleIndex = startIndex + Math.floor((startIndex + endIndex) >> 1);
+    // >> 运算符可以防止两个数太大而溢出
     const middleIndex = startIndex + Math.floor((startIndex + endIndex) / 2);
 
     if (sortedArray[middleIndex] === seekElement) {
